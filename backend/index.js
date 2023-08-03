fe3dd943c6dd7b4a7e0902bdbe6b6b61a82864c8
satyampandeyhcc <satyampandeyhcc@gmail.com>
2023-08-03 18:24:42 +0530

    FoodExpress

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,13 @@ const app = express();
 const path = require("path");
 
 const port = 5000;
+app.use(cors(
+  {
+    origin:["https://deploy-mern-1whq.vercel.app"],
+    methods:["POST","GET"],
+    credentials:true
+  }
+))
 // In nodejs we are use the keywords require rather than import.
 const mongoDB = require("./db");
 // mongoDB();
@@ -18,6 +25,7 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+mongoose.connect("mongodb://foodExpress:Satyam123@ac-z6u5csa-shard-00-00.cmvvd4f.mongodb.net:27017,ac-z6u5csa-shard-00-01.cmvvd4f.mongodb.net:27017,ac-z6u5csa-shard-00-02.cmvvd4f.mongodb.net:27017/foodExpress?ssl=true&replicaSet=atlas-lsvrwm-shard-0&authSource=admin&retryWrites=true&w=majority")
 app.use("/api", require("./Routes/CreateUser"));
 app.use("/api", require("./Routes/DisplayData"));
 app.use("/api", require("./Routes/OrderData"));
